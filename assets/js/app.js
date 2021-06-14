438cbf4cbbe9496a02dca6ee435605333b881c7f
Talha Akhter <talhaakhter01@gmail.com>
2021-06-14 17:48:21 +0500

    mobile responseive  updated

---
+++ assets/js/app.js
@@ -0,0 +1,79 @@
+const data = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1,
+    }],
+};
+
+const config = {
+    type: 'pie',
+    data: data,
+    options: {
+        legend: {
+            position: 'bottom',
+            labels: {
+                // fontColor: "white",
+                boxWidth: 20,
+                padding: 20
+            }
+        },
+        responsive: true,
+        plugins: {
+
+            // legend: {
+            //     display: true,
+            //     position: 'bottom',
+            // },
+            // align: 'end',
+            // title: {
+            //     display: true,
+            //     text: 'Chart.js Pie Chart',
+            // },
+
+            // datalabels: {
+            //     formatter: Math.round,
+            //     font: {
+            //         weight: 'bold',
+            //         size: 30,
+            //     },
+            //     color: '#ffffff',
+            // },
+
+            labels: {
+                render: 'percentage',
+                position: 'border',
+                precision: 1,
+                showZero: true,
+                fontSize: 30,
+                fontColor: '#FFF',
+                fontStyle: 'bold',
+                fontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+                showActualPercentages: true,
+                textMargin: 40,
+            }
+        },
+    },
+};
+
+var myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+);
